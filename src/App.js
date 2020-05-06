30065649b789288b24dcd73fbe68f62de353e8a1
3ncastro <3ncastro@gmail.com>
2020-05-06 02:43:59 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./components/Login/login";
+import index from "./components/calendar";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={Login} exact />
+          <Route path="/calendar" component={index} exact />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
